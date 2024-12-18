0e18c4066160ca3469949faaeb2b2d94ab4da415
ya-man777 <38710212+ya-man777@users.noreply.github.com>
2024-12-18 20:37:02 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "features/layer_lock.h"
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
