0d7b304cd4ba3338b29d933ba0752a5b68c7c207
ya-man777 <38710212+ya-man777@users.noreply.github.com>
2024-12-20 00:55:52 +0900

    Update keyball.c
    AML_TIMEOUT_MAX　500

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -30,7 +30,7 @@ const uint8_t CPI_MAX        = pmw3360_MAXCPI + 1;
 const uint8_t SCROLL_DIV_MAX = 7;
 
 const uint16_t AML_TIMEOUT_MIN = 100;
-const uint16_t AML_TIMEOUT_MAX = 1000;
+const uint16_t AML_TIMEOUT_MAX = 500;
 const uint16_t AML_TIMEOUT_QU  = 50;   // Quantization Unit
 const uint16_t AML_ACTIVATE_THRESHOLD = 50;
 
